4da4239ce9a5321d3d4d773ce81f175b28122d61
Irina Popova <irisha9753@gmail.com>
2021-11-14 17:40:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+count_of_redirects = len(response.history) - 1
+print(f"count_of_redirects = {count_of_redirects}")
+print(f"end url = {response.history[count_of_redirects].url}")
