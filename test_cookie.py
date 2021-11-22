4e1c0d585734a70d23eef3d3ff1023a8ad26dd68
Irina Popova <irisha9753@gmail.com>
2021-11-22 14:23:39 +0300

    Update test_cookie.py

--- test_cookie.py
+++ test_cookie.py
@@ -8,7 +8,7 @@ class TestCookie:
     def test_cookie(self):
         response1 = requests.post("https://playground.learnqa.ru/api/homework_cookie")
         print(response1.cookies)
-        assert "HomeWork" in response1.cookies, "There is no domain_re in response1 cookie"
+        assert "HomeWork" in response1.cookies, "There is no HomeWork in response1 cookie"
         expected_homeWork = "hw_value"
         actual_homeWorkth_sid = response1.cookies.get('HomeWork')
         assert expected_homeWork==actual_homeWorkth_sid, "HomeWork cookie is wrong"
