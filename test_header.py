82c2821b2752442e23c0154be8a66ee05dd54eea
Irina Popova <irisha9753@gmail.com>
2021-11-22 14:59:30 +0300

    Update test_header.py

--- test_header.py
+++ test_header.py
@@ -11,4 +11,4 @@ class TestHeader:
         assert "x-secret-homework-header" in response1.headers, "There is no x-secret-homework-header in response1 headers"
         expected_header = "Some secret value"
         actual_header = response1.headers.get("x-secret-homework-header")
-        assert expected_header == actual_header, "Header value cookie is wrong"
+        assert expected_header == actual_header, "Header value is wrong"
